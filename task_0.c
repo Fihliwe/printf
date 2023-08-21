acdcc888aec615d4dcc80cea1eecc85e7c1c1372
Fihliwe <siyabulelmncube@gmail.com>
2023-08-21 16:39:56 -0700

    fix

---
+++ task_0.c
@@ -0,0 +1,68 @@
+#include "main.h"
+
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int chars = 0;
+	char x;
+	char *strr;
+
+	va_start(args, format);
+
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+
+			if (*format == '\0')
+				break;
+
+			if (*format == 'c')
+			{
+				x = (char)va_arg(args, int);
+				putchar(x);
+				chars++;
+			}
+
+			else if (*format == 's')
+			{
+				strr = va_arg(args, char *);
+				while (*strr)
+				{
+					putchar(*strr);
+					strr++;
+					chars++;
+				}
+			}
+
+			else if (*format == '%')
+			{
+				putchar('%');
+				chars++;
+			}
+
+			else
+			{
+				putchar('%');
+				putchar(*format);
+				chars += 2;
+			}
+
+		}
+
+		else
+		{
+			putchar(*format);
+			chars++;
+		}
+
+		format++;
+
+	}
+
+	va_end(args);
+
+	return (chars);
+
+}
